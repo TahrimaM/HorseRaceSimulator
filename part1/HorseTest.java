c79161ff2c4fb8a72192fd116277ca7ff2dfc286
tahrimam <tahrimamilky141@gmail.com>
2025-04-24 20:49:30 +0100

    Testing race file

--- part1/HorseTest.java
+++ part1/HorseTest.java
@@ -1,34 +1,16 @@
 public class HorseTest {
         public static void main(String[] args) {
             // Create a new horse
-            Horse horse = new Horse('♘', "TAHRIMAS HORSE", 1.0000001);
+            HorseSRC horse1 = new HorseSRC('♘', "PIPPI LONGSTOCKING", 0.9);
+            HorseSRC horse2 = new HorseSRC('♞', "KOKOMO", 0.85);
+            HorseSRC horse3 = new HorseSRC('♕', "EL JEFE", 0.95);
 
-            // Print initial state
-            System.out.println("Name: " + horse.getName()); // Should be TAHRIMAS HORSE
-            System.out.println("Symbol: " + horse.getSymbol()); // Should be ♘
-            System.out.println("Confidence: " + horse.getConfidence()); // Should be 1
-            System.out.println("Distance Travelled: " + horse.getDistanceTravelled()); // Should be 0
-            System.out.println("Has Fallen: " + horse.hasFallen()); // Should be false
+            Race race = new Race(20);
+            race.addHorse(horse1, 1);
+            race.addHorse(horse2, 2);
+            race.addHorse(horse3, 3);
 
-            // Move forward
-            horse.moveForward();
-            System.out.println("Distance Travelled after moving: " + horse.getDistanceTravelled()); // Should be 1
+            race.startRace();
 
-            // Set confidence
-            horse.setConfidence(5);
-            System.out.println("Updated Confidence: " + horse.getConfidence()); // Should be 1 and show error message
-
-            // Set new symbol
-            horse.setSymbol('♞');
-            System.out.println("Updated Symbol: " + horse.getSymbol()); // Should be ♞
-
-            // Horse has not fallen, check if still false
-            System.out.println("Has Fallen: " + horse.hasFallen()); // Should be true
-            System.out.println("Updated Symbol: " + horse.getSymbol());
-
-            // Go back to start
-            horse.goBackToStart();
-            System.out.println("Distance after goBackToStart: " + horse.getDistanceTravelled()); // Should be 0
-            System.out.println("Has Fallen after goBackToStart: " + horse.hasFallen()); // Should be false
         }
 }
